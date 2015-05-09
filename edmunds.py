6b2885f569b42672b4b7175f8e430133178d0a5f
Dan Goldin <dangoldin@gmail.com>
2015-05-08 23:51:46 -0400

    Better sleep

--- edmunds.py
+++ edmunds.py
@@ -3,16 +3,20 @@
 import requests
 import json
 import time
+import random
 
 from config import EDMUNDS_KEY, EDMNUDS_SECRET
 
 MAX_ATTEMPTS = 5
 
+SLEEP_BASE = 2
+SLEEP_RAND = 5
+
 def get_styles(make, model, year, attempts = MAX_ATTEMPTS):
     url = 'https://api.edmunds.com/api/vehicle/v2/{0}/{1}/{2}?fmt=json&api_key={3}'.format(make, model, year, EDMUNDS_KEY)
     r = requests.get(url)
     if r.status_code == 403 and attempts > 0: # Retry
-        time.sleep(2)
+        time.sleep(SLEEP_BASE + random.randint(0, SLEEP_RAND))
         return get_styles(make, model, year, attempts - 1)
     if r.status_code != 200:
         print 'Status', r.status_code, r.content
@@ -24,7 +28,7 @@ def get_price(style_id, attempts = MAX_ATTEMPTS):
     url = 'https://api.edmunds.com/v1/api/tmv/tmvservice/calculateusedtmv?styleid={0}&condition=Outstanding&mileage=25000&zip=07302&fmt=json&api_key={1}'.format(style_id, EDMUNDS_KEY)
     r = requests.get(url)
     if r.status_code == 403 and attempts > 0: # Retry
-        time.sleep(2)
+        time.sleep(SLEEP_BASE + random.randint(0, SLEEP_RAND))
         return get_price(style_id, attempts - 1)
     if r.status_code != 200:
         print 'Status', r.status_code, r.content
